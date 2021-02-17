9ee22fecfdbf9e09f3c6d3661df84d19b9945000
ZsoltAntal9999 <antalzsolt2001@gmail.com>
2021-02-17 18:25:10 +0100

    Tésztaüzem

---
+++ Tesztauzem/tesztauzem.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener('load',init)
+
+function init(){
+    document.getElementById("gomb").addEventListener("click",torol_kiir)
+}
+
+function torol_kiir(){
+    document.getElementById("jatek-valasz").value="";
+    alert("Köszönjük, hogy részt vesz a játékunkban!");
+}
